4e694287a23e66542b13cc918454ad10145dea00
CHughes <Catherinehughes0317@gmail.com>
2025-02-07 14:51:23 -0600

    Added "WASD" Switch

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            
+            Switch();
+        }
+        static void Switch()
+        {
+            Console.WriteLine("Input a WASD key, then press enter.");
+            string? caseString = Console.ReadLine();
+            caseString = caseString?.ToUpper();
+           
+            switch (caseString)
+            {
+                case "W":
+                    Console.WriteLine("You moved up");
+                    break;
+                case "A":
+                    Console.WriteLine("You moved left");
+                    break;
+                case "S":
+                    Console.WriteLine("You moved down");
+                    break;
+                case "D":
+                    Console.WriteLine("You moved Right");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+
+            } 
+
         }
     }
 }
+
